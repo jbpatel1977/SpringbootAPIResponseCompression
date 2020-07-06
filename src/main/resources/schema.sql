7866ac02aaeef832c52de184e36e4137c283de30
Jiteshkumar Babubhai Patel(jiteshkumar.patel) <jiteshkumar.patel@infosys.com>
2020-07-06 11:32:42 +0530

    Added database scripts

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,3 @@
 
--- CREATE DATABASE IF NOT EXISTS genesis1;
--- USE genesis1;
-
-
--- -- MySQL  database query
-CREATE TABLE genesis1.employee (
-	employee_id INT(15) NOT NULL,
-	name VARCHAR(50) NOT NULL,
-	department_id INT(15) NOT NULL,
-	salary INT(15) NOT NULL,
-	
-	PRIMARY KEY (employee_id)
-	) ;
- 
+CREATE DATABASE IF NOT EXISTS genesis1;
+USE genesis1;
